7587f0e7e82e2f0add0892cb9a73e99faa5e3d9f
yoahn <yoahn.l@me.com>
2020-12-21 16:54:18 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c6181002b610ed374ad72daf81d429d8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6a1c6369-3a77-4db9-8d65-d1fcac71e7c1'
+};
